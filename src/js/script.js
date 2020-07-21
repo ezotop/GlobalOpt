469917439d4d872634cf2c2f60089c9c54546db3
Ezotop <valentinevdovenko@gmail.com>
2020-07-21 21:37:35 +0300

    non compressed

--- src/js/script.js
+++ src/js/script.js
@@ -29,8 +29,64 @@ $(document).ready(function() {
             }]
     });
 
+    $('#callbackme, .button_count').on('click', function() {
+        $('.overlay, #callback').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation_modal, #callback, #thanks').fadeOut('slow');
+    });
+    $('.details').each(function(i) {
+        $(this).on('click', function() {
+            $('#consultation_modal .modal__header').text($('.prices-item__header').eq(i).text());
+            $('.overlay, #consultation_modal').fadeIn('slow');
+        })
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "", //Введите имя
+                phone: "", //Введите номер
+                email: {
+                  required: "", //Введите эл. почту
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#contact form');
+    validateForms('#consultation_modal form');
+    validateForms('#callback form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #callback').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
     $(window).scroll(function() {
-        if ($(this).scrollTop()> 1600) {
+        if ($(this).scrollTop()> 1200) {
             $('.pageup').fadeIn();
         } else {
             $('.pageup').fadeOut();
